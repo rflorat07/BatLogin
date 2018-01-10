49b9d69ae8bef8fab217dd33dac1a7f21aef57a8
Roger Florat <rflorat@cidiverte.it>
2018-01-10 15:21:14 +0100

    LaunchScreen Animations

--- BatLogin/BatLogin/ViewController.swift
+++ BatLogin/BatLogin/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgLogoBatman: UIImageView!
+    @IBOutlet weak var lblInstructions: UILabel!
+    
+    var viewAnimator : UIViewPropertyAnimator!
+    private let unlockGesture = UIPanGestureRecognizer()
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        unlockGesture.addTarget(self, action: #selector(handle(pan:)))
+        self.view.addGestureRecognizer(unlockGesture)
+        
+        viewAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut, animations: nil)
+        viewAnimator.addAnimations {
+            self.lblInstructions.layer.opacity = 0
+            self.imgLogoBatman.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func handle(pan: UIPanGestureRecognizer) {
+        
+        let speed : CGFloat = 2.0
+        
+        switch pan.state {
+        case .began:
+            viewAnimator.pauseAnimation()
+        case .changed:
+            let translation = pan.translation(in: pan.view).y / speed
+            viewAnimator.fractionComplete = translation / 100
+            if viewAnimator.fractionComplete >= 0.99 {
+                buildAnimation()
+            }
+        default:
+            break
+        }
     }
-
-
+    
+    func buildAnimation() {
+        
+        let logoAnimator = UIViewPropertyAnimator(duration: 0.5, curve: .easeIn) {
+            self.imgLogoBatman.transform = CGAffineTransform(scaleX: 25.0, y: 25.0)
+        }
+        
+        logoAnimator.startAnimation()
+        logoAnimator.addCompletion { (_)  in
+            self.beginApp()
+        }
+        
+    }
+    
+    func beginApp(){
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+        self.present(loginViewController!, animated: true, completion: nil)
+    }
+    
 }
 
